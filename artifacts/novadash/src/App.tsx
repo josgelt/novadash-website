1d7646c4c521d9e40d5af9c3e7c70fc5a67c0ef9
josgelt <josef.gelter@gmail.com>
2026-04-18 22:19:41 +0200

    Initial commit: NovaDash SaaS landing page

---
+++ artifacts/novadash/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-black">NOVADASH SaaS</h1>
+    </div>
+  );
+}
+
+export default App;
